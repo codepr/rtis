a926a21dbf670fe92db4ce15add8fab06b2283de
Andrea <a.g.baldan@gmail.com>
2019-11-04 16:41:21 +0100

    Add HTTP method parsing

--- src/server.rs
+++ src/server.rs
@@ -3,6 +3,13 @@ use std::net::{TcpListener, TcpStream};
 
 type Addr = (String, u32);
 
+enum HTTP {
+    Get,
+    Post,
+    Put,
+    Delete
+}
+
 pub struct Server {
     addr: Addr
 }
@@ -20,8 +27,31 @@ impl Server {
     }
 }
 
+fn parse_request(request_method: &str) -> HTTP {
+    if request_method == "GET" {
+        return HTTP::Get;
+    }
+    if request_method == "POST" {
+        return HTTP::Post;
+    }
+    if request_method == "PUT" {
+        return HTTP::Put;
+    }
+    return HTTP::Delete;
+}
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let req_method = request
+        .split_whitespace()
+        .next()
+        .unwrap_or("");
+    match parse_request(&req_method) {
+        HTTP::Get => println!("GET"),
+        HTTP::Post => println!("POST"),
+        HTTP::Put => println!("PUT"),
+        HTTP::Delete => println!("DELETE")
+    }
 }
