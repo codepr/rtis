5fe5953b4dd1bdcf1bf017ee611c94e531369954
Andrea <a.g.baldan@gmail.com>
2019-11-05 11:21:05 +0100

    Better handling of errors in request parsing, still a bit of mess

--- src/server.rs
+++ src/server.rs
@@ -15,9 +15,14 @@ enum HTTP {
 }
 
 #[derive(Debug)]
+enum HTTPError {
+    NotFound = 404,
+    MethodNotAllowed = 405
+}
+
+#[derive(Debug)]
 struct Request {
     method: HTTP,
-    route: String,
     headers: HashMap<String, String>,
     body: String
 }
@@ -39,7 +44,7 @@ impl Server {
     }
 }
 
-fn parse_request(request: &str) -> Request {
+fn parse_request(request: &str) -> Result<Request, HTTPError> {
     let valid_methods: HashMap<&str, HTTP> =
         [("GET", HTTP::Get),
          ("POST", HTTP::Post),
@@ -51,27 +56,58 @@ fn parse_request(request: &str) -> Request {
         .split_whitespace()
         .next()
         .unwrap_or(""));
+    let route = match reqfields[0].split(" ").nth(1) {
+        Some(r) => {
+            if r == "/" {
+                None
+            } else {
+                Some(r)
+            }
+        },
+        None => None
+    };
     let mut headers: HashMap<String, String> = HashMap::new();
     let hdr_lines: Vec<&str> = reqfields[0].split("\r\n").collect();
     for i in 1..hdr_lines.len() {
         let kv: Vec<&str> = hdr_lines[i].split(":").collect();
         headers.insert(kv[0].to_string(), kv[1].to_string());
     }
-    println!("{}", reqfields[1].to_string());
-    let req: Request = Request {
-        method: *method.unwrap(),
-        route: reqfields[0].split(" ").nth(1).unwrap().to_string(),
-        headers: headers,
-        body: reqfields[1].trim_matches(char::from(0)).to_string()
+    return match route {
+        Some(_) => Err(HTTPError::NotFound),
+        None => {
+            match method {
+                Some(m) => {
+                    Ok(
+                        Request {
+                            method: *m,
+                            headers: headers,
+                            body: reqfields[1].trim_matches(char::from(0)).to_string()
+                        }
+                    )
+                },
+                None => Err(HTTPError::MethodNotAllowed)
+            }
+        }
     };
-
-    return req;
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     let reqstring = String::from_utf8_lossy(&buffer[..]);
-    let request = parse_request(&reqstring);
-    println!("{:?}", request);
+    let request = match parse_request(&reqstring) {
+        Ok(_) => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        },
+        Err(e) => {
+            let response = match e {
+                HTTPError::NotFound => "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+                HTTPError::MethodNotAllowed => "HTTP/1.1 405 METHOD NOT ALLOWED\r\n\r\n"
+            };
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+    };
 }
